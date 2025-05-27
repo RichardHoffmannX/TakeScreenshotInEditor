28a7fe462de2f1070bb3ae91f7ee2e66fe58cbce
Richard Hoffmann <95046844+RichardHoffmannX@users.noreply.github.com>
2025-05-27 09:33:49 +0200

    Update TakeScreenshotInEditor.cs

--- Assets/Editor/TakeScreenshotInEditor.cs
+++ Assets/Editor/TakeScreenshotInEditor.cs
@@ -4,29 +4,29 @@ using UnityEditor;
 
 public class TakeScreenshotInEditor : ScriptableObject
 {
-	public static string fileName = "Editor Screenshot ";
+    public static string fileName = "Editor Screenshot ";
     public static int startNumber = 1;
 
-	[MenuItem ("Custom/Take Screenshot of Game View %^t")]
-	static void TakeScreenshot()
-	{
-		int number = startNumber;
-		string name = "" + number;
+    [MenuItem ("Custom/Take Screenshot of Game View %^t")]
+    static void TakeScreenshot()
+    {
+	int number = startNumber;
+	string name = "" + number;
 
         string fileNameFull = fileName + name + ".png";
 
         while (System.IO.File.Exists(fileNameFull))
-		{
-			number++;
-			name = "" + number;
-		}
-
-		startNumber = number + 1;
-
-		ScreenCapture.CaptureScreenshot(fileNameFull);
-
-		Debug.Log($"TakeScreenshotInEditor: {fileNameFull}");
-		Application.OpenURL("file://" + Application.dataPath.Replace("Assets", ""));
+	{
+		number++;
+		name = "" + number;
 	}
+
+	startNumber = number + 1;
+
+	ScreenCapture.CaptureScreenshot(fileNameFull);
+
+	Debug.Log($"TakeScreenshotInEditor: {fileNameFull}");
+	Application.OpenURL("file://" + Application.dataPath.Replace("Assets", ""));
+     }
 }
 #endif
